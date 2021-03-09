81241d848f144e567846457b55cfbfc20c4e6f73
Trang Doan <doantranghp2000@gmail.com>
2021-03-10 00:45:42 +0900

    Add front-end components

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+//SET UP BACK-END
 const express = require('express')
 require('./db/mongoose')
 
@@ -15,6 +16,21 @@ app.use(express.json())
 app.use(userRouter)
 app.use(gistRouter)
 
+
+//SET UP FRONT-END
+const hbs = require('hbs')
+//Define paths for express config
+const viewPath = path.join(__dirname, '../templates/views')
+const publicDirPath = path.join(__dirname, '../public')
+
+//set up handlebars engine and view location
+app.set('view engine', 'hbs')
+app.set('views', viewPath)
+
+//set up static directory to serve
+app.use(express.static(publicDirPath))
+
 app.listen(port, () => {
     console.log("Server is up on port " + port)
 })
+
